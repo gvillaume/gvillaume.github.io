fab79ac80b08ee20bb354172763685b1edd4bf8a
George Villaume <39601888+gvillaume@users.noreply.github.com>
2021-06-23 23:54:34 -0500

    update siteUrl in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,48 +1,49 @@
 module.exports = {
-  siteMetadata: {
-    title: "gvillaume.github.io",
-  },
-  plugins: [
-    {
-      resolve: "gatsby-source-contentful",
-      options: {
-        accessToken: "c8zR_3QjaioqJ3AhPD04bkvsUBwksHZvjpSMsU9HSP4",
-        spaceId: "cqiijl66plgj",
-      },
+    siteMetadata: {
+        title: 'gvillaume.github.io',
+        siteUrl: 'https://gvillaume.github.io/',
     },
-    "gatsby-plugin-image",
-    {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: "G-9MVVX55KEZ",
-      },
-    },
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
-    },
-    "gatsby-plugin-mdx",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-    },
-  ],
-};
+    plugins: [
+        {
+            resolve: 'gatsby-source-contentful',
+            options: {
+                accessToken: 'c8zR_3QjaioqJ3AhPD04bkvsUBwksHZvjpSMsU9HSP4',
+                spaceId: 'cqiijl66plgj',
+            },
+        },
+        'gatsby-plugin-image',
+        {
+            resolve: 'gatsby-plugin-google-analytics',
+            options: {
+                trackingId: 'G-9MVVX55KEZ',
+            },
+        },
+        'gatsby-plugin-react-helmet',
+        'gatsby-plugin-sitemap',
+        {
+            resolve: 'gatsby-plugin-manifest',
+            options: {
+                icon: 'src/images/icon.png',
+            },
+        },
+        'gatsby-plugin-mdx',
+        'gatsby-plugin-sharp',
+        'gatsby-transformer-sharp',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'images',
+                path: './src/images/',
+            },
+            __key: 'images',
+        },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'pages',
+                path: './src/pages/',
+            },
+            __key: 'pages',
+        },
+    ],
+}
